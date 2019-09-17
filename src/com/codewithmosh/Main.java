eb395a9a977799f929f68d90486071011f7545cd
colm29 <colm29@gmail.com>
2019-09-17 16:48:30 +0100

    Reverse a string with stack

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -5,13 +5,10 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        var list = new LinkedList();
-        list.addLast(10);
-        list.addLast(20);
-        list.addLast(30);
-        list.addLast(40);
-        list.addLast(50);
-        var array = list.getMiddleNode();
-        System.out.println(list.getMiddleNode());
+        String str = null;
+        StringReverser reverser = new StringReverser();
+        var result = reverser.reverse(str);
+
+        System.out.println(result);
     }
 }
