8f37aac6173d0aa24c8bb0b5529deaa39bdc4ce7
colm29 <colm29@gmail.com>
2019-09-18 13:39:01 +0100

    Final Refactoring Check Balanced String with stack

--- src/com/codewithmosh/Expression.java
+++ src/com/codewithmosh/Expression.java
@@ -1,21 +1,40 @@
 package com.codewithmosh;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class Expression {
+
+    private final List<Character> leftBrackets = Arrays.asList('(','<','[','{');
+    private final List<Character> rightBrackets = Arrays.asList(')','>',']','}');
+
     public boolean isBalanced(String s) {
         Stack<Character> stack = new Stack<>();
 
         for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '<' || ch == '[' || ch == '{')
+            if (isLeftBracket(ch))
                 stack.push(ch);
-            else if (ch == ')' || ch == '>' || ch == ']') {
+            
+            else if (isRightBracket(ch)) {
                 if (stack.empty()) return false;
-                var top = stack.pop();
-                if ((top == '(' && ch != ')') || (top == '<' && ch != '>') || (top == '[' && ch != ']') || (top == '{' && ch != '}'))  return false;
-            }
-            }
-        return  stack.empty();
 
+                var top = stack.pop();
+                return bracketsMatch(top,ch);
+            }
+        }
+        return  stack.empty();
+    }
+
+    private boolean isLeftBracket(char ch) {
+        return   leftBrackets.contains(ch);
+    }
+
+    private boolean isRightBracket(char ch) {
+        return rightBrackets.contains(ch);
+    }
+
+    private  boolean bracketsMatch(char left, char right) {
+        return leftBrackets.indexOf(left) == rightBrackets.indexOf((right));
     }
 }
